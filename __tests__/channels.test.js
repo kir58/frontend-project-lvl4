66f098195cb0f176ef2d3ad7bb741e78c14340d6
Kirill Bushmin <>
2020-04-13 12:57:33 +0300

    fix test

--- __tests__/channels.test.js
+++ __tests__/channels.test.js
@@ -75,7 +75,7 @@ test('delete /channels/:id', async () => {
     method: 'DELETE',
     url: buildUrl('channels/100'),
   });
-  expect(response.statusCode).toEqual(204);
+  expect(response.statusCode).toEqual(200);
 });
 
 test('patch /channels/:id', async () => {
